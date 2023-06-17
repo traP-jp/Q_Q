80801541db33031e21f9f1c7f5f129204d306696
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2023-06-18 03:16:32 +0900

    ✨ mock作成を環境変数に逃した

--- q_q/core/config.py
+++ q_q/core/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     db_password: str
     db_database: str
     api_token: str
+    gen_mock_data: bool = False
 
     class Config:
         env_file = ".env"
@@ -19,6 +20,7 @@ class Settings(BaseSettings):
             "db_password": {"env": "NS_MARIADB_PASSWORD"},
             "db_database": {"env": "NS_MARIADB_DATABASE"},
             "api_token": {"env": "TRAQ_API_TOKEN"},
+            "gen_mock_data": {"env": "GEN_MOCK_DATA"},
         }
 
 
--- q_q/database/init_db.py
+++ q_q/database/init_db.py
@@ -1,3 +1,4 @@
+from q_q.core.config import Settings
 from q_q.database import mockdata
 from sqlalchemy.orm import sessionmaker
 from .session import engine
@@ -5,9 +6,13 @@ from .base import SQLBase
 
 
 def init_db() -> None:
-    print("init_db")
-    SessionClass = sessionmaker(engine)  # セッションを作るクラスを作成
-    session = SessionClass()
-    SQLBase.metadata.drop_all(bind=engine)
-    SQLBase.metadata.create_all(bind=engine)
-    mockdata.insert_mockdata(session)
+    if Settings.gen_mock_data:
+        print("init_db")
+        SessionClass = sessionmaker(engine)  # セッションを作るクラスを作成
+        session = SessionClass()
+        SQLBase.metadata.drop_all(bind=engine)
+        SQLBase.metadata.create_all(bind=engine)
+        mockdata.insert_mockdata(session)
+
+    else:
+        print("skip init_db")
